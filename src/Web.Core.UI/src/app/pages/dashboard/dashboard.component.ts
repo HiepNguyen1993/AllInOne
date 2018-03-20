5e841f8905df854a5241ddb4b5515b40afcdcf88
Hiep.Nguyen <hiep.nguyen@restaff.no>
2018-03-20 15:18:03 +0700

    update some things :D

--- src/Web.Core.UI/src/app/pages/dashboard/dashboard.component.ts
+++ src/Web.Core.UI/src/app/pages/dashboard/dashboard.component.ts
@@ -23,25 +23,25 @@ export class DashboardComponent implements OnInit {
                 this._script.loadScripts('head', ['assets/vendors/custom/fullcalendar/fullcalendar.bundle.js']);
             });
         this._router.events.subscribe((route) => {
-            if (route instanceof NavigationStart) {
-                (<any>mLayout).closeMobileAsideMenuOffcanvas();
-                (<any>mLayout).closeMobileHorMenuOffcanvas();
-                (<any>mApp).scrollTop();
-                Helpers.setLoading(true);
-                // hide visible popover
-                (<any>$('[data-toggle="m-popover"]')).popover('hide');
-            }
-            if (route instanceof NavigationEnd) {
-                // init required js
-                (<any>mApp).init();
-                (<any>mUtil).init();
-                Helpers.setLoading(false);
-                // content m-wrapper animation
-                let animation = 'm-animate-fade-in-up';
-                $('.m-wrapper').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(e) {
-                    $('.m-wrapper').removeClass(animation);
-                }).removeClass(animation).addClass(animation);
-            }
+            // if (route instanceof NavigationStart) {
+            //     (<any>mLayout).closeMobileAsideMenuOffcanvas();
+            //     (<any>mLayout).closeMobileHorMenuOffcanvas();
+            //     (<any>mApp).scrollTop();
+            //     Helpers.setLoading(true);
+            //     // hide visible popover
+            //     (<any>$('[data-toggle="m-popover"]')).popover('hide');
+            // }
+            // if (route instanceof NavigationEnd) {
+            //     // init required js
+            //     (<any>mApp).init();
+            //     (<any>mUtil).init();
+            //     Helpers.setLoading(false);
+            //     // content m-wrapper animation
+            //     let animation = 'm-animate-fade-in-up';
+            //     $('.m-wrapper').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(e) {
+            //         $('.m-wrapper').removeClass(animation);
+            //     }).removeClass(animation).addClass(animation);
+            // }
         });
   }
 
